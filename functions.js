0cb6e3c848485dc77f9a5ac8abd077568eb9f0f6
CNamgyel_12 <101409484+C-Namgyel@users.noreply.github.com>
2024-02-11 08:29:26 +0600

    Add files via upload

--- functions.js
+++ functions.js
@@ -91,3 +91,51 @@ function compressImage(file, compressor, code) {
         })
     }
 }
+function sortObject(data, sortBy, aToZ) {
+    data = Object.entries(data);
+    data.sort((a, b) => {
+        let valA = a[1][sortBy].toLowerCase();
+        let valB = b[1][sortBy].toLowerCase();
+        if (aToZ) {
+            return valA.localeCompare(valB);
+        } else {
+            return valB.localeCompare(valA);
+        }
+    });
+    let sortedObject = {};
+    data.forEach(entry => {
+        sortedObject[entry[0]] = entry[1];
+    });
+    return sortedObject;
+}
+function previousDate(time) {
+    let tempDate = time.split("-");
+    let year = tempDate[0];
+    let month = tempDate[1];
+    let date = tempDate[2];
+    date -= 1;
+    if (date <= 0) {
+        month -= 1;
+        console.log(month)
+        if ([1,3,5,7,8,10,12].includes(month)) {
+            date = 31;
+        } else if ([4,6,9,11].includes(month)) {
+            date = 30;
+        } else {
+            if (year % 4 == 0) {
+            date = 29;
+            } else {
+            date = 28
+            }
+        }
+    };
+    if (month <= 0) {
+        year -= 1;
+    };
+    return(`${year}-${month.toString().padStart(2, "0")}-${date.toString().padStart(2, "0")}`)
+}
+function timeToWords(time) {
+    let tempTime = time.split('-');
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return(`${months[parseInt(tempTime[1]) - 1]} ${tempTime[2]}, ${tempTime[0]}`)
+}
